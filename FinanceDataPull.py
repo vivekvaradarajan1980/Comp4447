d78fc7df22d7a9fa76e3190a58d656b7eaa5cf98
vivekvaradarajan1980 <varadarajan.vivek@gmail.com>
2023-01-15 07:20:10 -0600

    finance api plotter with ticker as a command argument, just as we see on google search!!

--- FinanceDataPull.py
+++ FinanceDataPull.py
@@ -1,9 +1,18 @@
 import yfinance as yf
 import pandas as pd
 from matplotlib.pylab import  *
+import argparse
 
 # pull data from specific ticker symbol over a specified period with a specified resolution interval
-data = yf.download('IBM',period='730d',interval='1h')
+
+parser = argparse.ArgumentParser(
+                    prog = 'Stock information',
+                    description = 'Plots closing stock over last 2 years with 1 hour resolution'
+                    )
+
+parser.add_argument('-t','--ticker',type=str,required=True)
+ticker= parser.parse_args().ticker;
+data = yf.download(ticker,period='730d',interval='1h')
 
 #data index is basically date information...
 
