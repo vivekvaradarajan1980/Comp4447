1e3f32c5217a2d554d7ab40817a89903a7d8f9bd
vivekvaradarajan1980 <varadarajan.vivek@gmail.com>
2023-01-22 12:59:15 -0600

    added period (how far back info) as an optional argument for the plotter

--- FinanceDataPull.py
+++ FinanceDataPull.py
@@ -13,12 +13,13 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-t','--ticker',type=str,required=False, default="GOOG")
 parser.add_argument('-o','--option',type=str,required=False,default="Close")
+parser.add_argument('-p','--period',type=str,required=False,default="2y")
 
 
 ticker= parser.parse_args().ticker
 option = parser.parse_args().option
-
-data = yf.download(ticker,period='730d',interval='1h')
+period= parser.parse_args().period
+data = yf.download(ticker,period=period,interval='1d')
 
 #data index is basically date information...
 
