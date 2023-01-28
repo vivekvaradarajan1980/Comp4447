85b349c11ddf9ef0bcbe557d15e7a79bd559cf87
vivekvaradarajan1980 <varadarajan.vivek@gmail.com>
2023-01-27 19:56:13 -0600

    simple front end that dumps the yfinance data as a table

---
+++ App.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template, url_for
+import yfinance as yf
+import pandas as pd
+
+from matplotlib.pylab import *
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+
+data = pd.DataFrame({})
+
+
+@app.route('/new/', methods=['GET', 'POST'])
+def welcome():
+    return "Hello World!"
+
+
+@app.route('/ticker/<symbol>', methods=(['GET']))
+def tickerdata(symbol):
+    data = yf.download(symbol, period='max', interval='1d')
+    return data.to_html()
+
+
+@app.route('/finance/', methods=(['POST','GET']))
+def getTicker():
+    if request.method == "POST":
+        ticker = request.form['ticker']
+
+        return redirect(url_for('tickerdata', symbol=ticker))
+    else:
+        # if the user refreshes the form by accident just re-render
+        return render_template('YFinanceFrontEnd.html')
+
+
+# our home route
+@app.route('/')
+def index():
+    return render_template('YFinanceFrontEnd.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
